fd0c224aacd4769af0d07fd8039187273af0f689
sanrvallejo <zanti-23@hotmail.com>
2024-11-11 22:57:31 -0500

    servicios: interface e implementacion de servicios de Producto

---
+++ server/src/main/java/com/gavi/server/services/IProductoService.java
@@ -0,0 +1,21 @@
+package com.gavi.server.services;
+
+import com.gavi.server.model.Producto;
+
+import java.util.List;
+
+public interface IProductoService {
+    Producto obtenerPorId(Long id);
+
+    List<Producto> obtenerProductos();
+
+    Producto obtenerPorCodigo(String codigo);
+
+    Producto obtenerPorNombre(String nombre);
+
+    Producto crarProducto(Producto nuevoProducto);
+
+    void editarProducto(Long id, Producto productoEditado);
+
+    void eliminarProducto(Long id);
+}
---
+++ server/src/main/java/com/gavi/server/services/ProductoService.java
@@ -0,0 +1,72 @@
+package com.gavi.server.services;
+
+import com.gavi.server.model.Producto;
+import com.gavi.server.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class ProductoService implements IProductoService{
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Override
+    public Producto obtenerPorId(Long id) {
+        return productoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Producto> obtenerProductos() {
+        return productoRepository.findAll();
+    }
+
+    @Override
+    public Producto obtenerPorCodigo(String codigo) {
+        return productoRepository.obtenerPorCodigo(codigo);
+    }
+
+    @Override
+    public Producto obtenerPorNombre(String nombre) {
+        return productoRepository.obtenerPorNombre(nombre);
+    }
+
+    @Override
+    public Producto crarProducto(Producto nuevoProducto) {
+        return productoRepository.save(nuevoProducto);
+    }
+
+    @Override
+    public void editarProducto(Long id, Producto productoEditado) {
+        Producto productoEncontrado = productoRepository.findById(id).orElse(null);
+        if (productoEncontrado!= null) {
+            if (productoEncontrado.getCodigo() != null) {
+                productoEncontrado.setCodigo(productoEditado.getCodigo());
+            }
+
+            if (productoEncontrado.getNombre() != null) {
+                productoEncontrado.setNombre(productoEditado.getNombre());
+            }
+
+            if (productoEncontrado.getPrecio() != null) {
+                productoEncontrado.setPrecio(productoEditado.getPrecio());
+            }
+
+            if (productoEncontrado.getCantidad() != null) {
+                productoEncontrado.setCantidad(productoEditado.getCantidad());
+            }
+
+            productoRepository.save(productoEncontrado);
+        }else {
+            System.out.println("No se pudo editar el producto");;
+        }
+    }
+
+    @Override
+    public void eliminarProducto(Long id) {
+        productoRepository.deleteById(id);
+    }
+}

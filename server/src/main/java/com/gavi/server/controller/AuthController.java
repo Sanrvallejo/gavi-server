a59784e2a608a901036d86a02e414fa910b4683c
sanrvallejo <zanti-23@hotmail.com>
2024-11-19 23:10:37 -0500

    controlador: auth

---
+++ server/src/main/java/com/gavi/server/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.gavi.server.controller;
+
+import com.gavi.server.dto.LoginDto;
+import com.gavi.server.model.Usuario;
+import com.gavi.server.services.IUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/gavi/auth")
+public class AuthController {
+    @Autowired
+    private IUsuarioService usuarioService;
+
+    @PostMapping("/login")
+    public ResponseEntity<Usuario> login(LoginDto loginDto){
+        Optional<Usuario> usuarioEncontrado = usuarioService.obtenerPorEmailAndPass(loginDto.getEmail(), loginDto.getPassword());
+
+        if (usuarioEncontrado.isPresent()) {
+            return ResponseEntity.ok(usuarioEncontrado.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}

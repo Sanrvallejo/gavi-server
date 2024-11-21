70f3638fb53d06e5b92633cf5a58f03527d99ac4
sanrvallejo <zanti-23@hotmail.com>
2024-11-21 00:02:53 -0500

    ajuste: prefijo endpoint en RequestMapping

--- server/src/main/java/com/gavi/server/controller/AuthController.java
+++ server/src/main/java/com/gavi/server/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/gavi/auth")
+@RequestMapping("/api/gavi")
 public class AuthController {
     @Autowired
     private IUsuarioService usuarioService;

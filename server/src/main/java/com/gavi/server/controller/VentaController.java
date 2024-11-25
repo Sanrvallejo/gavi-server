5636d22451e827edc8b2594cc6cf183765380e5e
sanrvallejo <zanti-23@hotmail.com>
2024-11-25 18:40:37 -0500

    controlador: venta

---
+++ server/src/main/java/com/gavi/server/controller/VentaController.java
@@ -0,0 +1,9 @@
+package com.gavi.server.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/gavi")
+public class VentaController {
+}
